3b79b57e7a59aafe1d3559514e4da1b3a314c574
Henrique Pochmann <henriquepcm@gmail.com>
2024-10-04 15:48:26 -0300

    increase maximum duration for API function

--- app/api/updateSupabase/route.ts
+++ app/api/updateSupabase/route.ts
@@ -1,6 +1,9 @@
 import { UpdateSupabaseViaApi } from "@/scripts/updateSupabaseViaApi";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 15; // This function can run for a maximum of 15 seconds
+export const dynamic = "force-dynamic";
+
 export async function GET() {
      try {
           await UpdateSupabaseViaApi();
