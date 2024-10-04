d99bf8b4cb4fe3a0da83639b452ed7feec98977a
Henrique Pochmann <henriquepcm@gmail.com>
2024-10-04 13:39:11 -0300

    once again tring to fix cron job

--- pages/api/updateSupabase.ts
+++ pages/api/updateSupabase.ts
@@ -1,15 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import { UpdateSupabaseViaApi } from "@/scripts/updateSupabaseViaApi";
-import { NextResponse } from "next/server";
 
-export async function GET() {
-     try {
-          await UpdateSupabaseViaApi();
-          return NextResponse.json(
-               { message: "Update successful" },
-               { status: 200 }
-          );
-     } catch (error) {
-          console.error("Error running update script:", error);
-          return NextResponse.json({ error: "Script failed" }, { status: 500 });
+export default async function handler(
+     req: NextApiRequest,
+     res: NextApiResponse
+) {
+     if (req.method === "GET") {
+          try {
+               await UpdateSupabaseViaApi();
+               return res.status(200).json({ message: "Update successful" });
+          } catch (error) {
+               console.error("Error running update script:", error);
+               return res.status(500).json({ error: "Script failed" });
+          }
+     } else {
+          return res
+               .setHeader("Allow", ["GET"])
+               .status(405)
+               .end(`Method ${req.method} Not Allowed`);
      }
 }
