7947f41a6f93d0461571520e15e0c940e2b20fa4
Henrique Pochmann <henriquepcm@gmail.com>
2024-10-04 10:59:14 -0300

    fix cron job

--- app/api/updateSupabase/route.ts
+++ app/api/updateSupabase/route.ts
@@ -1,11 +1,11 @@
 import { UpdateSupabaseViaApi } from "@/scripts/updateSupabaseViaApi";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function GET() {
      try {
           await UpdateSupabaseViaApi();
           return NextResponse.json(
-               { message: "Update succesful" },
+               { message: "Update successful" },
                { status: 200 }
           );
      } catch (error) {
