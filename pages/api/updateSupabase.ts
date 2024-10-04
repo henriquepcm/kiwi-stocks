020b8857ae0369d146fcdb90712a5be8040d5d57
Henrique Pochmann <henriquepcm@gmail.com>
2024-10-04 13:23:58 -0300

    fix cron job malfunctioning

--- pages/api/updateSupabase.ts
+++ pages/api/updateSupabase.ts
@@ -1,20 +1,15 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { UpdateSupabaseViaApi } from "../../scripts/updateSupabaseViaApi";
+import { UpdateSupabaseViaApi } from "@/scripts/updateSupabaseViaApi";
+import { NextResponse } from "next/server";
 
-export default async function handler(
-     req: NextApiRequest,
-     res: NextApiResponse
-) {
-     if (req.method === "GET") {
-          try {
-               await UpdateSupabaseViaApi();
-               res.status(200).json({ message: "Data updated successfully." });
-          } catch (error) {
-               console.error("Error updating data:", error);
-               res.status(500).json({ error: "Failed to update data." });
-          }
-     } else {
-          res.setHeader("Allow", ["GET", "POST"]);
-          res.status(405).end(`Method ${req.method} Not Allowed`);
+export async function GET() {
+     try {
+          await UpdateSupabaseViaApi();
+          return NextResponse.json(
+               { message: "Update successful" },
+               { status: 200 }
+          );
+     } catch (error) {
+          console.error("Error running update script:", error);
+          return NextResponse.json({ error: "Script failed" }, { status: 500 });
      }
 }
