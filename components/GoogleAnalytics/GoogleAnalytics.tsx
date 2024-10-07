86305ce2ccf70603e800f15fc563a97d49c3fe3a
Henrique Pochmann <henriquepcm@gmail.com>
2024-10-07 14:08:27 -0300

    adjust to Google Analytics

--- components/GoogleAnalytics/GoogleAnalytics.tsx
+++ components/GoogleAnalytics/GoogleAnalytics.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import Script from "next/script";
 
 const GoogleAnalytics = () => {
+     const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
+     if (!GA_ID) {
+          console.warn("Google Analytics ID is missing.");
+          return null;
+     }
+
      return (
           <>
                <Script
                     strategy="lazyOnload"
-                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
                />
 
-               <Script id="" strategy="lazyOnload">
+               <Script id="google-analytics" strategy="lazyOnload">
                     {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+              gtag('config', '${GA_ID}', {
               page_path: window.location.pathname,
               });
           `}
