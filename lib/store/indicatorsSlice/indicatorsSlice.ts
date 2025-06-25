162ac8298f1cc03bc4c2fe8afc9bbd81f2d82b46
Henrique Pochmann <henriquepcm@gmail.com>
2025-06-25 15:20:17 -0300

    chore: set indicators as visible by default

--- lib/store/indicatorsSlice/indicatorsSlice.ts
+++ lib/store/indicatorsSlice/indicatorsSlice.ts
@@ -2,18 +2,18 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IndicatorsState } from "./indicatorsSliceTypes";
 
 const defaultIndicatorsState: IndicatorsState = {
-     symbol: { isVisible: false, isActive: false },
-     dividend_yield: { isVisible: false, isActive: false },
+     symbol: { isVisible: true, isActive: false },
+     dividend_yield: { isVisible: true, isActive: false },
      eps_forward: { isVisible: false, isActive: false },
-     eps_trailing_twelve_months: { isVisible: false, isActive: false },
-     fifty_two_week_range_low: { isVisible: false, isActive: false },
-     fifty_two_week_range_high: { isVisible: false, isActive: false },
-     forward_pe: { isVisible: false, isActive: false },
-     market_cap: { isVisible: false, isActive: false },
-     price_to_book: { isVisible: false, isActive: false },
-     regular_market_price: { isVisible: false, isActive: false },
-     regular_market_volume: { isVisible: false, isActive: false },
-     trailing_pe: { isVisible: false, isActive: false },
+     eps_trailing_twelve_months: { isVisible: true, isActive: false },
+     fifty_two_week_range_low: { isVisible: true, isActive: false },
+     fifty_two_week_range_high: { isVisible: true, isActive: false },
+     forward_pe: { isVisible: true, isActive: false },
+     market_cap: { isVisible: true, isActive: false },
+     price_to_book: { isVisible: true, isActive: false },
+     regular_market_price: { isVisible: true, isActive: false },
+     regular_market_volume: { isVisible: true, isActive: false },
+     trailing_pe: { isVisible: true, isActive: false },
 };
 
 const initialState = defaultIndicatorsState;
